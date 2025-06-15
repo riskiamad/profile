c19c2d86a975b43722fbb678977b112648a83c73
riskiamad <riskiamad@gmail.com>
2025-06-16 05:17:06 +0700

    fix error import

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react"
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
 import { Menu, X } from "lucide-react"
 import Image from "next/image"
 
